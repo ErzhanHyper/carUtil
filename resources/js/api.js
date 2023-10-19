9e016c5bfc8a64c7f0cbeb1920bc3e64a832f923
Ержан Шалдыбаев <erzhan.hype@gmail.com>
2023-10-19 22:48:15 +0600

    'vets1.0'

--- resources/js/api.js
+++ resources/js/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
     withCredentials: true,
-    baseURL: 'http://127.0.0.1:8000/app',
-    // baseURL: 'https://dev-auto.recycle.kz/app',
+    // baseURL: 'http://127.0.0.1:8000/app',
+    baseURL: 'https://dev-auto.recycle.kz/app',
     headers: {
         "X-Requested-With": "XMLHttpRequest",
         "Access-Control-Allow-Origin": "*",
