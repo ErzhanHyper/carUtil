a62398cce34fbdde040b220249d57b1d6511a3ef
Ержан Шалдыбаев <erzhan.hype@gmail.com>
2023-11-14 17:37:54 +0600

    'camera_audio'

--- resources/js/Pages/order/actions/OrderVideoAction.vue
+++ resources/js/Pages/order/actions/OrderVideoAction.vue
@@ -7,7 +7,13 @@
                icon="keyboard_return"
                v-if="permissions.showVideoRevision"
         />
-
+        <q-btn label="Отправить видеозапись"
+               color="blue-8"
+               icon="videocam"
+               square size="12px"
+               v-if="permissions.showVideoSendAction && showCameraBtn"
+               @click="cameraDialog = true"
+        />
     </div>
 
     <q-dialog
