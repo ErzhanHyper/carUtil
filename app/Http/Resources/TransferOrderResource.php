33e927fdd8bd31e9d2dd58d317eaa5ffa9efd12b
Ержан Шалдыбаев <erzhan.hype@gmail.com>
2023-11-28 11:48:26 +0600

    'vets2'

--- app/Http/Resources/TransferOrderResource.php
+++ app/Http/Resources/TransferOrderResource.php
@@ -45,14 +45,19 @@ class TransferOrderResource extends JsonResource
             }
         }
 
+        $deal = null;
         $amount = 0;
-        $deal = TransferDeal::where('liner_id', $auth->id)->where('transfer_order_id', $this->id)->first();
-        if($deal) {
-            $amount = $deal->amount;
-            $currentClient = Client::find($deal->client_id);
+        if($currentClient) {
+            $deal = TransferDeal::where('liner_id', $auth->id)->where('transfer_order_id', $this->id)->first();
+            if($deal) {
+                $currentClient = Client::find($deal->client_id);
+            }
         }
 
         $transferDealAccept = TransferDeal::where('transfer_order_id', $this->id)->where('id', $this->transfer_deal_id)->first();
+        if($transferDealAccept){
+            $amount = $transferDealAccept->amount;
+        }
 
         $isOwner = false;
         $canSign = false;
