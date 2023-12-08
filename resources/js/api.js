78fbc8879cfceb950f08e16b2ed7ac6bd2e3631d
Ержан Шалдыбаев <erzhan.hype@gmail.com>
2023-12-08 10:02:16 +0600

    init

--- resources/js/api.js
+++ resources/js/api.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
     withCredentials: true,
-    baseURL: 'http://localhost:8062/app',
-    // baseURL: 'https://dev-auto.recycle.kz/app',
+    baseURL: 'http://127.0.0.1:8000/app',
     headers: {
         "X-Requested-With": "XMLHttpRequest",
         "Access-Control-Allow-Origin": "*",
